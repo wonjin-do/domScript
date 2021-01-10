89d6e1ad5b776e174708052da98608fc4c2eb7d6
wonjin <wonjin@bizspring.co.kr>
2021-01-10 14:29:49 +0900

    update showPic.js

--- showPic.js
+++
@@ -1,8 +0,0 @@
-function showPic(whichpic) {
-    alert("HELLO");
-    var source = whichpic.getAttribute("href");
-    var placeholder = document.getElementById('placeholder');
-
-    placeholder.setAttribute("src", source);
-
-}
